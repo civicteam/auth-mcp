3f944758a18eca34f0b50fafd0562aa6008bbf41
mriedel <1713643+rado0x54@users.noreply.github.com>
2025-06-04 14:01:58 +0200

    feat: removed FileTokenPersistence.ts (will live in integrating codebase)

--- library/src/client/providers/persistence/index.ts
+++ library/src/client/providers/persistence/index.ts
@@ -1,3 +1,2 @@
 export { TokenPersistence } from "./TokenPersistence.js";
 export { InMemoryTokenPersistence } from "./InMemoryTokenPersistence.js";
-export { FileTokenPersistence } from "./FileTokenPersistence.js";
